8cf0bd110ca9cf8f6e64fe92382106aa2221861d
oleg <andreisungurovsky@yandex.ru>
2023-11-25 01:09:33 +0300

    Add view for login, new templates and css style. Add form for login

--- account/forms.py
+++ account/forms.py
@@ -2,5 +2,8 @@ from django import forms
 
 
 class LoginForm(forms.Form):
+    """
+    Form for login in the app
+    """
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
